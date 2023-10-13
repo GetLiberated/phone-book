910960470f33fac61d58868ec0214afae19ef927
Muhammad Erizky Suryaputra <43334192+GetLiberated@users.noreply.github.com>
2023-10-14 06:57:54 +0700

    Apollo GraphQL integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_CONTACT_LIST = gql`
+  query GetContactList (
+      $distinct_on: [contact_select_column!], 
+      $limit: Int, 
+      $offset: Int, 
+      $order_by: [contact_order_by!], 
+      $where: contact_bool_exp
+  ) {
+    contact(
+        distinct_on: $distinct_on, 
+        limit: $limit, 
+        offset: $offset, 
+        order_by: $order_by, 
+        where: $where
+    ){
+      created_at
+      first_name
+      id
+      last_name
+      phones {
+        number
+      }
+    }
+  }
+`;
+
+function DisplayContactList() {
+  const { loading, error, data } = useQuery(GET_CONTACT_LIST);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  interface IContact {
+    id: string;
+    first_name: string;
+    last_name: string;
+    phones: {
+      number: string;
+    }[];
+  }
+
+  return data.contact.map(({ id, first_name, last_name, phones }: IContact) => (
+    <div key={id}>
+      <h3>{first_name} {last_name}</h3>
+      <br />
+      <b>Phone:</b>
+      <p>{phones[0].number}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Phone Book</h2>
+      <br/>
+      <DisplayContactList />
     </div>
   );
 }
