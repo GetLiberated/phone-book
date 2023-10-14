99fa20fc1df51fd1da74855ab7003c03b16f7b40
Muhammad Erizky Suryaputra <43334192+GetLiberated@users.noreply.github.com>
2023-10-14 12:46:12 +0700

    Global type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+
+interface IContact {
+    id: string;
+    first_name: string;
+    last_name: string;
+    phones: {
+      number: string;
+    }[];
+}
+
+type IContacts = Array<IContact>
