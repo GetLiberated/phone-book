22f97636896f0d826baf230e321c0207cfd6180e
Muhammad Erizky Suryaputra <43334192+GetLiberated@users.noreply.github.com>
2023-10-15 20:09:03 +0700

    Create pagination max page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -35,7 +35,7 @@ export default function Pagination({ pageNumber, setPageNumber, contacts }: Page
             </svg>
         </button>
         <p>
-          {pageNumber}
+          {pageNumber} / {Math.ceil(contacts.length/10)}
         </p>
         <button onClick={() => {setPageNumber(pageNumber + 1)}} className={button} disabled={pageNumber*10 > contacts.length}>
             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-right" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
